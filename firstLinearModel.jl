e6b78b691a6fc1a402690e9f64efde98ce10504a
monsoonification <austraeohperhaps@gmail.com>
2025-01-29 16:37:26 -0600

    the model in question:

---
+++ firstLinearModel.jl
@@ -0,0 +1,16 @@
+# Top Brass Optimization Model for CS524
+
+
+
+using HiGHS
+using JuMP
+
+TBmodel = Model()
+set_optimizer(TBmodel, HiGHS.Optimizer)
+@variable(TBmodel, 1000 >= fTroph >= 0, Int)
+@variable(TBmodel, 1500 >= sTroph >= 0, Int)
+@constraint(TBmodel, 4 * fTroph + 2 * sTroph <= 4800)
+@constraint(TBmodel, fTroph + sTroph <= 1750)
+@objective(TBmodel, Max, 12 * fTroph + 9 * sTroph)
+
+optimize!(TBmodel)
